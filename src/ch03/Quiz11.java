730d408fa6ae306add2909fc1901d22c48488103
glory1223 <hilyk1223@gmail.com>
2024-02-08 17:26:56 +0900

    240208

--- src/ch03/Quiz11.java
+++ src/ch03/Quiz11.java
@@ -9,12 +9,12 @@ public class Quiz11 {
         char ch = 'A';
         ch = (char)(ch + 2);
 
-        float f = 3 / 2.0f; //정수 연산의 결과는 정수
+        float f = 3 / 2.0f; //정수 연산의 결과는 정수이기에 소수점과 f를 붙여 소수점까지 연산결과를 얻어야한다.
         long l = 3000 * 3000 * 3000L;
         float f2 = 0.1f;
 
-        double d = 0.1f;
-        boolean result = d==f2;
+        double d = 0.1;
+        boolean result = (float)d==f2; //double과 float은 연산시 반드시 타입을 맞춰줘야한다. 부동소수점 때문에
 
         System.out.println("c="+c);
         System.out.println("ch="+ch);
